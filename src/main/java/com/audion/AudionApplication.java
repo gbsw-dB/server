f47a0a6878bdc4ce6e061ed15d69a3c1a7fa11f0
YoungjinKim <talkingjuly@gmail.com>
2025-03-13 16:12:35 +0900

    feat: initialize

---
+++ src/main/java/com/audion/AudionApplication.java
@@ -0,0 +1,13 @@
+package com.audion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AudionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/audion/AudionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.audion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
