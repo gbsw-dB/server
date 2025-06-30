5b3d91f21b69773e844ce186f15b003f8fbe37e1
kwonyonghoon <biggrace681@gmail.com>
2025-06-30 19:44:17 +0900

    swaggerUI 명세서 수정

--- src/main/java/com/audion/audioanalysis/controller/AudioPassController.java
+++ src/main/java/com/audion/audioanalysis/controller/AudioPassController.java
@@ -2,6 +2,7 @@ package com.audion.audioanalysis.controller;
 
 import com.audion.audioanalysis.dto.AudioAnalysisResponse;
 import com.audion.audioanalysis.service.AudioAnalysisService;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -23,8 +24,10 @@ public class AudioPassController {
     }
 
     @Tag(name = "FastAPI upload", description = "FastAPI로 전송 및 분석 결과 받기")
-    @PostMapping("/upload")
-    public ResponseEntity<AudioAnalysisResponse> uploadAudio(@RequestParam("audio") MultipartFile file){
+    @PostMapping(value = "/upload", consumes = {"multipart/form-data"})
+    public ResponseEntity<AudioAnalysisResponse> uploadAudio(
+            @Parameter(description = "업로드할 오디오 파일", required = true)
+            @RequestParam("audio") MultipartFile file){
         try{
             // FastAPI로 전송 및 분석 결과 받기
             AudioAnalysisResponse response = audioAnalysisService.sendToFastAPI(
